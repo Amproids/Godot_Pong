cd95662c036bb246cd925c3945e9426f3bbcd022
Andrew Parry <parryandrew47@gmail.com>
2023-10-26 22:10:12 -0600

    fixed ball physics, very smooth

--- Game_Scene.gd
+++ Game_Scene.gd
@@ -2,15 +2,14 @@ extends Node3D
 var gameState
 var player1Score
 var player2Score
-var ballZ
 var player1Position = Vector3(0, 0, 0)
 var player2Position = Vector3(0, 0, 0)
+var worldTime = 0
 
 func _ready():
 	gameState = 0
 	player1Score = 0
 	player2Score = 0
-	ballZ = 0
 	
 func _process(delta):
 	if Input.is_action_just_pressed("pause_unpause"):
--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,8 +1,12 @@
 extends RigidBody3D
 var velocity = Vector3(-10, 0, 0)
+var PADDLEANGLERANGE = 120
+var PADDLEANGLESCALEFACTOR = (PADDLEANGLERANGE/2)/3
+var ballSpeedMax = 30
+var ballSpeed = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.z = GameScene.ballZ
+	position.z = 0
 	position.x = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -10,14 +14,25 @@ func _process(delta):
 	if GameScene.gameState:
 		var collisionInfo = move_and_collide(velocity * delta)
 		if collisionInfo:
-			velocity = velocity.bounce(collisionInfo.get_normal())
+			#update ball's positional relation to players. (I don't know how to pull this data from collisionInfo)
 			var DistanceToPlayer1 = GameScene.player1Position - position
 			var DistanceToPlayer2 = GameScene.player2Position - position
-			if abs(DistanceToPlayer1[0]) < 2 and abs(DistanceToPlayer1[2]) < 3:
-				velocity.z = -DistanceToPlayer1[2]*5
-				print("bounced off player 1")
-			if abs(DistanceToPlayer2[0]) < 2 and abs(DistanceToPlayer2[2]) < 3:
-				velocity.z = -DistanceToPlayer2[2]*5
-				print("bounced off player 2")
-			GameScene.player1Score = DistanceToPlayer1
-			GameScene.player2Score = DistanceToPlayer2
+			if abs(DistanceToPlayer1[0]) < 1.1 and abs(DistanceToPlayer1[2]) < 3:
+				var theta = -deg_to_rad(PADDLEANGLESCALEFACTOR * DistanceToPlayer1[2])
+				velocity.x = cos(theta) * ballSpeed
+				velocity.z = sin(theta) * ballSpeed
+				if ballSpeed < ballSpeedMax:
+					ballSpeed += 1
+			elif abs(DistanceToPlayer2[0]) < 1.1 and abs(DistanceToPlayer2[2]) < 3:
+				var theta = -deg_to_rad(PADDLEANGLESCALEFACTOR * DistanceToPlayer2[2])
+				velocity.x = -cos(theta) * ballSpeed
+				velocity.z = sin(theta) * ballSpeed
+				if ballSpeed < ballSpeedMax:
+					ballSpeed += 1
+			else:
+				velocity = velocity.bounce(collisionInfo.get_normal())
+			#If ball goes out of bounds.
+			if position.x < -18:
+				GameScene.player2Score += 1
+			if position.x > 18:
+				GameScene.player1Score += 1
